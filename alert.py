d1f55021af26a6e179e6516ac176a7c105b0bb32
Nate Koenig <55851808+NateKoenig@users.noreply.github.com>
2021-05-23 16:38:08 -0400

    Update alert.py

--- alert.py
+++ alert.py
@@ -16,41 +16,64 @@ def scrape():
 	page_soup = soup(page_html, "html.parser")
 
 	#grab price diff %
-	price = str(page_soup.findAll("span", {"class":"percent-value-text"})[0].text)
+	return str(page_soup.findAll("span", {"class":"percent-value-text"})[0].text)
 
-	#look at price % and decide what message to display
+def createMessage(price, lastPrice, curPriceUpOrDown, lasPriceUpOrDown):
+	#initialize variables
 	subject = "This is the subject"
 	body = "This is my message"
+	messageSomething = 1
 
-	if price[0] != '-':
-		price = float(price)
-		subject = "Yeah!"
-		if price >= 20:
+	#if the current and last price differ over the 0% price line, then we update
+	if curPriceUpOrDown != lasPriceUpOrDown:
+		if curPriceUpOrDown == "up":
+			subject = "LET'S GO!"
+			if price >= 20:
+				body = "DOGE is up over 20% today at {}%".format(price)
+			elif price >= 10:
+				body = "DOGE is up over 10% today at {}%".format(price)
+			elif price >= 5:
+				body = "DOGE is up over 5% today at {}%".format(price)
+			else:
+				body = "DOGE is up a bit today at {}%".format(price)
+		else:
+			subject = "OUCH!"
+			if price >= 20:
+				body = "DOGE is down over 20% today at {}%".format(price)
+			elif price >= 10:
+				body = "DOGE is down over 10% today at {}%".format(price)
+			elif price >= 5:
+				body = "DOGE is down over 5% today at {}%".format(price)
+			else:
+				body = "DOGE is down a bit today at {}%".format(price)
+	#otherwise if they're both positive, only message something if there's a new threshold
+	elif curPriceUpOrDown == "up":
+		if price >= 20 and lasPrice <20:
 			body = "DOGE is up over 20% today at {}%".format(price)
-		elif price >= 10:
+		elif price >= 10 and (lasPrice >=20 or lasPrice < 10):
 			body = "DOGE is up over 10% today at {}%".format(price)
-		elif price >= 5:
+		elif price >= 5 and (lasPrice >=10 or lasPrice < 5):
 			body = "DOGE is up over 5% today at {}%".format(price)
 		else:
-			body = "DOGE is up a bit today at {}%".format(price)
+			messageSomething = 0 #do nothing, no significant change
+	#otherwise, they're both negative, only message something if there's a new threshold
 	else:
-		price = float(price[1:])
-		subject = "Oops!"
-		if price >= 20:
+		if price >= 20 and lasPrice <20:
 			body = "DOGE is down over 20% today at {}%".format(price)
-		elif price >= 10:
+		elif price >= 10 and (lasPrice >= 20 or lasPrice < 10):
 			body = "DOGE is down over 10% today at {}%".format(price)
-		elif price >= 5:
+		elif price >= 5 and (lasPrice >= 10 or lasPrice < 5):
 			body = "DOGE is down over 5% today at {}%".format(price)
 		else:
-			body = "DOGE is down a bit today at {}%".format(price)
+			messageSomething = 0 #do nothing, no significant change
 
 	#make this an option later, for now send to me (verizon)
-	to = "your number/email goes here"
+	to = "the number/email you want to send to goes here"
 
 
-	#calls message alert
-	messageAlert(subject, body, to)
+	#calls message alert only if we need to message something
+	if messageSomething == 1:
+		messageAlert(subject, body, to)
 
 
 # Creates message alert
@@ -62,9 +85,9 @@ def messageAlert(subject, body, to):
 	msg['to'] = to
 
 	#new email
-	user = "your 'from' email"
+	user = "your sent from email goes here"
 	msg['from'] = user
-	password = "your email's app key"
+	password = "your sent from email app password goes here"
 
 
 	#necessary to be able to send email/text, like a server from gmail
@@ -79,8 +102,33 @@ def messageAlert(subject, body, to):
 
 # Driver
 if __name__ == '__main__':
-	while True: #Infinite loop (create count variable later if want to only run for a certain period of time)
-		scrape() #Execute the function, feeding it the last percentage
-		time.sleep(600) #Wait 600s (10 min) before re-entering the cycle
 
+	thing = "10.0"
+	thing2 = float(thing)
 
+	#create variables
+	price = "this is a price"
+	lastPrice = "100000.00"
+
+	#run infinitely
+	while True:
+		price = scrape()
+		savePrice = price
+
+		if price[0] != '-':
+			if lastPrice[0] != '-':
+				createMessage(float(price), float(lastPrice), "up", "up")
+			else:
+				lastPrice = float(lastPrice[1:])
+				createMessage(float(price), lastPrice, "up", "down")
+		else:
+			price = float(price[1:])
+			if lastPrice[0] != '-':
+				createMessage(price, float(lastPrice), "down", "up")
+			else:
+				lastPrice = float(lastPrice[1:])
+				createMessage(float(price), lastPrice, "down", "down")
+
+		lastPrice = savePrice #update last price
+
+		time.sleep(300) #wait 300s (5 min) before re-entering the cycle
