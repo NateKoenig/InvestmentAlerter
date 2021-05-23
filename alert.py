ed69d6dccebdab4f0f877f49000ae691a33d6b16
Nate Koenig <55851808+NateKoenig@users.noreply.github.com>
2021-05-23 15:36:42 -0400

    Update alert.py

--- alert.py
+++ alert.py
@@ -1,13 +1,8 @@
 import smtplib
 from email.message import EmailMessage
-
-#IF PROBLEMS, IMPORT THE ABOVE LIBRARIES
-
 from urllib.request import urlopen as uReq
-from bs4 import BeautifySoup as soup
-
-
-#TODO: make sure that it sends every time the price changes from what it currently is (ie from over 20 to over 10 etc)
+from bs4 import BeautifulSoup as soup
+import time
 
 def scrape():
 	my_url = 'https://www.coindesk.com/price/dogecoin'
@@ -21,49 +16,37 @@ def scrape():
 	page_soup = soup(page_html, "html.parser")
 
 	#grab price diff %
-	price = string(page_soup.findAll("span", {"class":"percent-value-text"}))
+	price = str(page_soup.findAll("span", {"class":"percent-value-text"})[0].text)
 
 	#look at price % and decide what message to display
 	subject = "This is the subject"
 	body = "This is my message"
 
-	print(price)
-
-	if (price[0] != '-') {
-		price = int(price)
+	if price[0] != '-':
+		price = float(price)
 		subject = "Yeah!"
-		if (price >= 20) {
+		if price >= 20:
 			body = "DOGE is up over 20% today at {}%".format(price)
-		}
-		else if (price >= 10) {
+		elif price >= 10:
 			body = "DOGE is up over 10% today at {}%".format(price)
-		}
-		else if (price >= 5) {
+		elif price >= 5:
 			body = "DOGE is up over 5% today at {}%".format(price)
-		}
-		else {
+		else:
 			body = "DOGE is down a bit today at {}%".format(price)
-		}
-	}
-	else {
-		price = price[1:]
+	else:
+		price = float(price[1:])
 		subject = "Oops!"
-		if (price >= 20) {
+		if price >= 20:
 			body = "DOGE is down over 20% today at {}%".format(price)
-		}
-		else if (price >= 10) {
+		elif price >= 10:
 			body = "DOGE is down over 10% today at {}%".format(price)
-		}
-		else if (price >= 5) {
+		elif price >= 5:
 			body = "DOGE is down over 5% today at {}%".format(price)
-		}
-		else {
+		else:
 			body = "DOGE is down a bit today at {}%".format(price)
-		}
-	}
 
 	#make this an option later, for now send to me (verizon)
-	to = "your email or number"
+	to = "enter number or email"
 
 
 	#calls message alert
@@ -79,9 +62,9 @@ def messageAlert(subject, body, to):
 	msg['to'] = to
 
 	#new email
-	user = "your email"
+	user = "enter 'from' email"
 	msg['from'] = user
-	password = "your app key"
+	password = "enter your app key from that email"
 
 
 	#necessary to be able to send email/text, like a server from gmail
@@ -94,8 +77,10 @@ def messageAlert(subject, body, to):
 
 
 
-scrape()
+# Driver
+if __name__ == '__main__':
+	while True: #Infinite loop (create count variable later if want to only run for a certain period of time)
+		scrape() #Execute the function, feeding it the last percentage
+		time.sleep(600) #Wait 60s (10 min) before re-entering the cycle
 
-## Test function, send this stuff to my number (if you want email, just replace w/ email to send to)
-#if __name___ == '__main__':
-#	messageAlert("Hey", "Hello world", "put your phone number or email here")
+
